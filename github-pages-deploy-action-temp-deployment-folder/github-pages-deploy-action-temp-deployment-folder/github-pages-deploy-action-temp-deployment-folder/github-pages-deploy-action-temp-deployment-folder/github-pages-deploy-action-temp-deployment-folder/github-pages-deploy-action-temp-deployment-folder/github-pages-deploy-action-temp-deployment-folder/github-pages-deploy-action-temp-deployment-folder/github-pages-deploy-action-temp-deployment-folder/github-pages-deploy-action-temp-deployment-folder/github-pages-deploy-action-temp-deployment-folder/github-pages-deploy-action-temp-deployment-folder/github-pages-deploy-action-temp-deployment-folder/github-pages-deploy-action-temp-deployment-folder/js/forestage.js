4ed084b9615f65bf47053d6d5bb3ec067ed462c2
stevecyj <ycsteven.tsao@gmail.com>
2021-12-10 23:56:47 +0000

    Deploying to gh-pages from @ stevecyj/js21FalW8-woworoom@ad5238c8fa227fcec42351ba2c291254df90d993 🚀

--- github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/js/forestage.js
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/js/forestage.js
@@ -1,19 +1,69 @@
-let productData = [];
+/**
+ * DOM
+ */
+
+// product list
+const productsWrap = document.querySelector("#productWrap");
+
+/**
+ * data
+ */
+
+let productsData = [];
 
 /**
  * get products
  */
 
-function getData() {
+function getProductsData() {
   axios
     .get(`${baseUrl}/customer/${api_path}/products`)
     .then(function (res) {
-      productData = res.data.products;
-      console.log(productData);
+      productsData = res.data.products;
+      // console.log(productsData);
+      renderProducts(productsData);
     })
     .catch(function (error) {
       console.log(error.response);
     });
 }
 
-window.onload = getData();
+// window.onload = getProductData();
+
+/**
+ * 千分位
+ */
+
+function toThousandComma(num) {
+  let parts = num.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+}
+
+/**
+ * render products list
+ */
+
+function renderProducts(data) {
+  // 渲染產品卡片
+  let cardStr = ``;
+  data.forEach((item) => {
+    let originPrice = toThousandComma(item.origin_price);
+    let price = toThousandComma(item.price);
+    cardStr += `
+    <li class="productCard">
+      <h4 class="productType">新品</h4>
+      <img src="${item.images}" alt="">
+      <a href="" class="addCart" data-productId=${item.id}>加入購物車</a>
+      <h3 class="titleOverFlow">${item.title}</h3>
+      <del class="originPrice">NT$${originPrice}</del>
+      <p class="nowPrice">NT$${price}</p>
+    </li>`;
+  });
+  productsWrap.innerHTML = cardStr;
+}
+
+/**
+ * initial
+ */
+getProductsData();
